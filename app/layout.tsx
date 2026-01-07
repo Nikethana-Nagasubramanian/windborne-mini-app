c6e0341876fc7b4ffa9ce5779a30266b725a7ecc
nike935 <nikethanan01@gmail.com>
2026-01-07 10:09:36 -0500

    Updated NextJs version for Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Wildborne Mini App',
+  description: 'Created with v0 and Cursor',
+  generator: 'c0 and Cursor',
   icons: {
     icon: [
       {
